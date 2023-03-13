4273b603cf51b8ff3deec9d41523494bd8a85601
jinzhao-hust <2427957775@qq.com>
2023-03-13 20:26:45 +0800

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,7 +3,7 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** implementation of nextNumber! */
     public static int nextNumber(int n) {
         if (n>>1 == 1){
             return n * 3 + 1;
